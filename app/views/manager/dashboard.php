e30a425349f137a5c6faae003a618325bd60b681
Samindu <2020cs025@stu.ucsc.cmb.ac.lk>
2022-11-11 14:44:05 +0530

    Dashboard
    Navigation and Top Bar Components

--- app/views/manager/dashboard.php
+++ app/views/manager/dashboard.php
@@ -1,12 +1,12 @@
-<?php
-
-
-    require_once APP_ROOT . '\views\includes\header.php';
- ?>
-<h1>Manager Dashboard</h1>
+<?php require_once APP_ROOT . '\views\includes\header.php'; ?>
+<?php require_once APP_ROOT . '\views\includes\navbar.php'; ?>
+<!-- <h1>Manager Dashboard</h1>
+<a href="<?php echo URL_ROOT; ?>managers/logout">logout</a>
 <?php
     print_r($_SESSION);
 ?>
-<br>
-<a href="<?php echo URL_ROOT; ?>managers/logout">logout</a>
+<br> -->
+
+<script type="text/javascript" src="<?php echo URL_ROOT;?>public/javascripts/main.js"></script>
+
 
