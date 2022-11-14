184acd4d2f04ddcd89bc2a6b2315c65831910c78
BhanukaRajk <bhanukayar@gmail.com>
2022-11-15 02:23:56 +0530

    Initial files

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+    /* APP ROOT */
+    define('APP_ROOT',dirname(dirname(__FILE__)));
+
+    /* URL ROOT */
+    const URL_ROOT = 'http://localhost/MicroCAPS/';
+
+    /* SITE NAME */
+    const SITE_NAME = 'MicroCAPS';
+
+    /* DATABASE */
+    const DB_HOST = 'localhost';
+    const DB_USER = 'root';
+    const DB_PASS = '';
+    const DB_NAME = 'microcaps';
