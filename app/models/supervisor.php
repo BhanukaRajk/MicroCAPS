5cb122b81a3fcdd094b3ee1bb73b7ce71cf2f5d7
BhanukaRajk <bhanukayar@gmail.com>
2022-11-15 16:47:09 +0530

    Initial model

---
+++ app/models/supervisor.php
@@ -0,0 +1,45 @@
+<?php
+class Supervisor {
+    private $db;
+
+    public function __construct(){
+        $this->db = new Database;
+    }
+
+    public function findUserByUsername($username) {
+
+        $this->db->query('SELECT * FROM credentials  WHERE credentials.Username = :username');
+
+        $this->db->bind(':username', $username);
+
+        $row = $this->db->single();
+
+        if ($this->db->rowCount()) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public function login($username,$password) {
+        $this->db->query(
+            'SELECT credentials.Username, credentials.Password, employee.EmployeeID, employee.Firstname, employee.Lastname, employee.Position
+            FROM credentials
+            INNER JOIN employee
+            ON credentials.EmployeeID = employee.EmployeeId
+            WHERE credentials.Username = :username'
+        );
+
+        $this->db->bind(':username', $username);
+
+        $row = $this->db->single();
+
+        if ( $password == $row->Password ) {
+            return $row;
+        } else {
+            return null;
+        }
+    }
+
+}
