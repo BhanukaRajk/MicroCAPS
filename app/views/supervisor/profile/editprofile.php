e0e0f539302c6cb47343b552816fe862439c1d5c
Bhanuka Rajakaruna <87462362+BhanukaRajk@users.noreply.github.com>
2023-02-06 11:43:22 +0530

    editprofile changed

--- app/views/supervisor/profile/editprofile.php
+++ app/views/supervisor/profile/editprofile.php
@@ -3,7 +3,7 @@
 <?php require_once APP_ROOT . '/views/supervisor/common/leftnavbar.php'; ?>
 <?php require_once APP_ROOT . '/views/supervisor/common/topnavbar.php'; ?>
 
-<section class = "position-absolute page-content-2" >
+<section class = "position-absolute page-content" >
     <div class="page-heading  font-weight">
         Account Settings
     </div>
@@ -11,7 +11,7 @@
     <div class="display-flex-row align-items-center gap-10 border-radius-1 background-white paddingx-6 paddingy-5 height-rem-25 margin-top-4">
         <div class="display-flex-column align-items-center justify-content-center margin-left-5">
             <div class="img-grid">
-                <img src="<?php echo URL_ROOT; ?>public/images/profile/<?php echo $data['userDetails']->Image; ?>" class="border-radius-11 width-rem-12p5" alt="profilepic" id="img-preview"/>
+                <div style="background-image:url(<?php echo URL_ROOT; ?>public/images/profile/<?php echo $data['userDetails']->Image; ?>)" class="border-radius-11 width-rem-12p5 height-rem-12p5 background-image" title="profilepic" id="img-preview"></div>
                 <img src="<?php echo URL_ROOT; ?>public/images/add.png" class="grid-add width-rem-2p5"/>
             </div>
             <div class="page-heading font-size-24 font-weight margin-top-4">
@@ -46,7 +46,7 @@
                             name="firstname"
                             onChange=""
                             value="<?php echo $data['userDetails']->Firstname; ?>"
-                            class="form-control form-control-blue text-fontgray width-rem-20"
+                            class="form-control form-control-blue text-fontgray width-rem-25"
                             placeholder="Firstname"
                             autocomplete="off"
                             disabled
@@ -59,7 +59,7 @@
                             name="lastname"
                             onChange=""
                             value="<?php echo $data['userDetails']->Lastname; ?>"
-                            class="form-control form-control-blue text-fontgray width-rem-20"
+                            class="form-control form-control-blue text-fontgray width-rem-25"
                             placeholder="Lastname"
                             disabled />
                         <label class="form-label blue">Last Name</label>
@@ -70,7 +70,7 @@
                             name="email"
                             onChange=""
                             value="<?php echo $data['userDetails']->Email; ?>"
-                            class="form-control form-control-blue text-fontgray width-rem-20"
+                            class="form-control form-control-blue text-fontgray width-rem-25"
                             placeholder="Email"
                             disabled />
                         <label class="form-label blue">Email</label>
@@ -81,7 +81,7 @@
                             name="mobile"
                             onChange=""
                             value="<?php echo $data['userDetails']->TelephoneNo; ?>"
-                            class="form-control form-control-blue text-fontgray width-rem-20"
+                            class="form-control form-control-blue text-fontgray width-rem-25"
                             placeholder="mobile"
                             disabled />
                         <label class="form-label blue">Mobile Number</label>
@@ -92,7 +92,7 @@
                             name="nic"
                             onChange=""
                             value="<?php echo $data['userDetails']->NIC; ?>"
-                            class="form-control form-control-blue text-fontgray width-rem-20"
+                            class="form-control form-control-blue text-fontgray width-rem-25"
                             placeholder="nic"
                             disabled />
                         <label class="form-label blue">NIC</label>
@@ -105,19 +105,11 @@
 
 <section class="display-flex-column">
 
-    <div class="alert fade alert-success" id="alert-success" role="alert">
-        <i class="icon fa-check-circle margin-right-3"></i>
-        Success
-    </div>
-
-    <div class="alert fade alert-failure" id="alert-faliure" role="alert">
-        <i class="icon fa-times-circle margin-right-3"></i>
-        Falied
-    </div>
+    <div id="alert" class="hideme" role="alert"></div>
 
 </section>
 
 
-<script type="text/javascript" src="<?php echo URL_ROOT;?>public/javascripts/main.js"></script>
+<script type="module" src="<?php echo URL_ROOT;?>public/javascripts/main.js"></script>
 <script type="text/javascript" src="<?php echo URL_ROOT;?>public/javascripts/cors.js"></script>
 <script type="text/javascript" src="<?php echo URL_ROOT;?>public/javascripts/settings.js"></script>
