5638329e7ea5c7b6489def9405f72ff3ddd38ed7
Bhanuka Rajakaruna <87462362+BhanukaRajk@users.noreply.github.com>
2023-02-07 16:10:34 +0530

    Unnecessary files has deleted

--- public/javascripts/testerjs/main.js
+++
@@ -1,7 +0,0 @@
-const drop = document.getElementById("drop");
-const logout = document.getElementById("logout");
-
-drop.addEventListener("click", () => {
-    console.log("clicked");
-    logout.classList.toggle("show");
-});
