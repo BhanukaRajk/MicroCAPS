64a3d5f9678e52640c6e5e9a0fd13c6025b984cc
Rusiru Rathmina <rusirurathminarr@gmail.com>
2023-02-07 17:15:18 +0530

    Changes

--- app/config/config.php
+++ app/config/config.php
@@ -1,16 +1,36 @@
 <?php
 
-    /** App Root */
-    define('APP_ROOT',dirname(dirname(__FILE__)));
+    /* APP ROOT */
+    define('APP_ROOT', dirname(dirname(__FILE__)));
 
-    /** URL Root */
+
+    /* URL ROOT FOR BHANUKA*/
+    // const URL_ROOT = 'http://localhost:8888/MicroCAPS/';
+
+    /* URL ROOT FOR SAMINDU*/
     const URL_ROOT = 'http://localhost/MicroCAPS/';
 
-    /** Site Name */
+
+    /* SITE NAME */
     const SITE_NAME = 'MicroCAPS';
 
-    /** Database */
+
+    /* DATABASE CONNECTION FOR LOCAL SERVER ON BHANUKA'S MACBOOK AIR */
+    // const DB_HOST = 'localhost';
+    // const DB_USER = 'root';
+    // const DB_PASS = 'root';
+    // const DB_NAME = 'microcaps';
+
+    /* DATABASE CONNECTION FOR LOCAL SERVER ON SAMINDU'S PC */
     const DB_HOST = 'localhost';
     const DB_USER = 'root';
     const DB_PASS = '';
     const DB_NAME = 'microcaps';
+
+    /* DATABASE CONNECTION FOR CLOUD SERVER */
+    // const DB_HOST = 'microcaps-db.co36o1syoru0.ap-northeast-1.rds.amazonaws.com';
+    // const DB_USER = 'admin';
+    // const DB_PASS = 'admin123';
+    // const DB_NAME = 'microcaps';
+
+?>
