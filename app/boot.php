eb3f92502c3bf7677f1976780a27bd4a573b3dbf
Bhanuka Rajakaruna <87462362+BhanukaRajk@users.noreply.github.com>
2023-02-07 17:40:19 +0530

    bott file updated

--- app/boot.php
+++ app/boot.php
@@ -3,10 +3,17 @@
     /* Config */
     require_once 'config/config.php';
 
+    /* Helpers */
+    require_once 'helpers/DateHelper.php';
+    require_once 'helpers/EmailHelper.php';
     require_once 'helpers/UrlHelper.php';
     require_once 'helpers/SessionHelper.php';
 
     /* Libraries */
     spl_autoload_register( function( $className ) {
         require_once 'libraries/'.$className.'.php';
+    });
+
+    spl_autoload_register( function( $className ) {
+        require_once 'PHPMailer/src/'.$className.'.php';
     });
