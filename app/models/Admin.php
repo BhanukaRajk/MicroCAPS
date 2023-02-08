26f540339482d4daecd0e022453769371b125946
Samindu <2020cs025@stu.ucsc.cmb.ac.lk>
2023-02-08 23:32:35 +0530

    Admin Changes
    Admin Dashboard and Employee Done By Dilmi

--- app/models/Admin.php
+++ app/models/Admin.php
@@ -6,71 +6,30 @@ class Admin {
         $this->db = new Database;
     }
 
-    public function findUserByUsername($username) {
+    public function userDetails($position) {
+        $this->db->query('SELECT * FROM Employee WHERE Position = :position');
 
-        $this->db->query('SELECT * FROM credentials  WHERE credentials.Username = :username');
+        $this->db->bind(':position', $position);
 
-        $this->db->bind(':username', $username);
+        $row = $this->db->resultSet();
 
-        $row = $this->db->single();
+        if ( $row ) {
+            return $row;
+        } else {
+            return null;
+        }
+    }
 
-        if ($this->db->rowCount()) {
+    public function userDelete($id): bool
+    {
+        $this->db->query('DELETE FROM Employee WHERE EmployeeId = :id');
+
+        $this->db->bind(':id', $id);
+
+        if ( $this->db->execute() ) {
             return true;
         } else {
             return false;
-        }
-
-    }
-
-    public function login($username,$password) {
-
-        $this->db->query('SELECT Username, Password FROM credentials  WHERE credentials.Username = :username');
-
-        $this->db->bind(':username', $username);
-
-        $row = $this->db->single();
-
-        if ( $password == $row->Password ) {
-            return $row;
-        } else {
-            return null;
-        }
-
-    }
-
-    public function userdetails() {
-        $this->db->query('SELECT * FROM Employee WHERE Position = "Manager"');
-
-        $row = $this->db->resultSet();
-
-        if ( $row ) {
-            return $row;
-        } else {
-            return null;
-        }
-    }
-
-    public function userdetails_2() {
-        $this->db->query('SELECT * FROM Employee WHERE Position = "Supervisor"');
-
-        $row = $this->db->resultSet();
-
-        if ( $row ) {
-            return $row;
-        } else {
-            return null;
-        }
-    }
-
-    public function userdetails_3() {
-        $this->db->query('SELECT * FROM Employee WHERE Position = "Tester"');
-
-        $row = $this->db->resultSet();
-
-        if ( $row ) {
-            return $row;
-        } else {
-            return null;
         }
     }
 
---
+++ public/javascripts/adminjs/cors.js
@@ -0,0 +1,90 @@
+
+// Flash Message
+$(document).ready(() => {
+
+    if (getItem("FlashState") === "Successful") {
+        alertSuccess(getItem("FlashMessage"));
+    } else if (getItem("FlashState") === "Error") {
+        alertFaliure(getItem("FlashMessage"));
+    }
+
+    removeLocalStorage()
+
+})
+
+// C.O.R.S
+function deleteEmployee(id) {
+    
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = this.responseText;
+
+            if (response == "Successful") {
+
+                location.reload();
+                setLocalStorage("Successful","Employee " + id + " is Successfully Deleted");
+                
+
+            } else {
+                location.reload();
+                setLocalStorage("Error","Deletion Unsuccessful");
+
+            }
+
+        }
+    };
+    xhttp.open("POST", "http://localhost/MicroCAPS/Admins/employees", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("id="+id);
+
+}
+
+//Alert Success
+function alertSuccess(message) {
+    let alert = document.getElementById("alert");
+    alert.classList.remove("hideme");
+    alert.classList.add("shows");
+    alert.classList.add("showme");
+    alert.classList.add("alert-success");
+    alert.innerHTML = "<i class='icon fa-check-circle margin-right-3'></i>"+message;
+
+    setTimeout(() => { 
+        alert.classList.add("hideme");
+        alert.classList.remove("shows");
+        alert.classList.remove("showme");
+        alert.classList.remove("alert-success");
+    }, 5000);
+}
+
+//Alert Faliure
+function alertFaliure(message) {
+    let alert = document.getElementById("alert");
+    alert.classList.remove("hideme");
+    alert.classList.add("shows");
+    alert.classList.add("showme");
+    alert.classList.add("alert-failure");
+    alert.innerHTML = "<i class='icon fa-times-circle margin-right-3'></i>"+message;
+
+    setTimeout(() => { 
+        alert.classList.add("hideme");
+        alert.classList.remove("shows");
+        alert.classList.remove("showme");
+        alert.classList.remove("alert-failure");
+    }, 5000);
+}
+
+//Local Storage
+function setLocalStorage(FlashState,FlashMessage) {
+    localStorage.setItem("FlashState",FlashState);
+    localStorage.setItem("FlashMessage",FlashMessage);
+}
+
+function getItem(key) {
+    return localStorage.getItem(key);
+}
+
+function removeLocalStorage() {
+    localStorage.removeItem("FlashState");
+    localStorage.removeItem("FlashMessage");
+}
