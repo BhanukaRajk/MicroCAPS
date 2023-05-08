8bba83262793796578d18faa47e22c0fae244e5a
Bhanuka Rajakaruna <87462362+BhanukaRajk@users.noreply.github.com>
2023-05-08 16:01:01 +0530

    Admin's missed JS files added

---
+++ public/javascripts/adminjs/cors.js
@@ -0,0 +1,155 @@
+
+// Flash Message
+$(document).ready(() => {
+
+    if (getItem("FlashState") === "Successful") {
+        alertSuccess(getItem("FlashMessage"));
+    } else if (getItem("FlashState") === "Error") {
+        alertFaliure(getItem("FlashMessage"));
+    }
+
+    removeLocalStorage()
+
+})
+
+// C.O.R.S
+function addEmployee() {
+    let form = document.getElementById("add-employee");
+    let formData = new FormData(form);
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost/MicroCAPS/admins/employees/add',
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: (response) => {
+            if (response == "Successful") {
+                location.reload(true);
+                setLocalStorage("Successful","Employee Added Successfully");
+            } else {
+                location.reload();
+                setLocalStorage("Error","Error Adding Employee");
+            }
+        }
+    });
+}
+
+function editEmployee($id) {
+    let form = document.getElementById("edit-employee");
+    let formData = new FormData(form);
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost/MicroCAPS/admins/employees/edit/'+$id,
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: (response) => {
+            if (response == "Successful") {
+                location.reload(true);
+                setLocalStorage("Successful","Employee Added Successfully");
+            } else {
+                location.reload();
+                setLocalStorage("Error","Error Adding Employee");
+            }
+        }
+    });
+}
+
+function deleteEmployee($id) {
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = this.responseText;
+
+            if (response == "Successful") {
+
+                location.reload();
+                setLocalStorage("Successful","Employee Deleted Successfully");
+
+            } else {
+                
+                location.reload();
+                setLocalStorage("Error","Employee Deletion Failed");
+
+            }
+
+        }
+    };
+    xhttp.open("POST", "http://localhost/MicroCAPS/admins/employees/delete", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("id="+$id);
+}
+
+function saveChanges(id, position) {
+    let formdata = new FormData();
+    formdata.append("id", id);
+    formdata.append("image", document.getElementById("image").files[0]);
+    formdata.append("firstname", document.getElementById("firstname").value);
+    formdata.append("email", document.getElementById("email").value);
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost/MicroCAPS/'+position+'s/settings',
+        data: formdata,
+        processData: false,
+        contentType: false,
+        success: (response) => {
+            if (response == "Successful") {
+                location.reload(true);
+                setLocalStorage("Successful","Saved Changes");
+            } else {
+                location.reload();
+                setLocalStorage("Error","Error Saving Changes");
+            }
+        }
+    });
+}
+
+//Alert Success
+function alertSuccess(message) {
+    let alert = document.getElementById("alert");
+    alert.classList.remove("hideme");
+    alert.classList.add("shows");
+    alert.classList.add("showme");
+    alert.classList.add("alert-success");
+    alert.innerHTML = "<i class='icon fa-check-circle margin-right-3'></i>"+message;
+
+    setTimeout(() => { 
+        alert.classList.add("hideme");
+        alert.classList.remove("shows");
+        alert.classList.remove("showme");
+        alert.classList.remove("alert-success");
+    }, 5000);
+}
+
+//Alert Faliure
+function alertFaliure(message) {
+    let alert = document.getElementById("alert");
+    alert.classList.remove("hideme");
+    alert.classList.add("shows");
+    alert.classList.add("showme");
+    alert.classList.add("alert-failure");
+    alert.innerHTML = "<i class='icon fa-times-circle margin-right-3'></i>"+message;
+
+    setTimeout(() => { 
+        alert.classList.add("hideme");
+        alert.classList.remove("shows");
+        alert.classList.remove("showme");
+        alert.classList.remove("alert-failure");
+    }, 5000);
+}
+
+//Local Storage
+function setLocalStorage(FlashState,FlashMessage) {
+    localStorage.setItem("FlashState",FlashState);
+    localStorage.setItem("FlashMessage",FlashMessage);
+}
+
+function getItem(key) {
+    return localStorage.getItem(key);
+}
+
+function removeLocalStorage() {
+    localStorage.removeItem("FlashState");
+    localStorage.removeItem("FlashMessage");
+}
