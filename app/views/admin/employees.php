8e85d4dd2f43a0875af0204bd969ae5ea39731da
Samindu <2020cs025@stu.ucsc.cmb.ac.lk>
2023-05-11 17:43:14 +0530

    Employee

--- app/views/admin/employees.php
+++ app/views/admin/employees.php
@@ -18,165 +18,164 @@
 
     <div class="page-content-2">
         <?php
-        if ($data['managerDetail'] != null) {
-            echo '<div class="profile-detail-board  margin-bottom-4">
-                    <div class="profile-data-board">';
+            if ($data['managerDetail'] != null) {
+                echo '<div class="profile-detail-board  margin-bottom-4">
+                        <div class="profile-data-board">';
 
-            foreach ($data['managerDetail'] as $value) {
+                foreach ($data['managerDetail'] as $value) {
 
-                echo '<div class="profile-card">
-                        <div class="display-flex-row justify-content-between">
-                            <div class="profile-img">
-                                <div style="background-image:url(' . URL_ROOT . 'public/images/profile/' . $value->Image . ')" class="width-rem-8p5 height-rem-8p5 background-image border-radius-11"></div>
+                    echo '<div class="profile-card">
+                            <div class="display-flex-row justify-content-between">
+                                <div class="profile-img">
+                                    <div style="background-image:url(' . URL_ROOT . 'public/images/profile/' . $value->Image . ')" class="width-rem-8p5 height-rem-8p5 background-image border-radius-11"></div>
+                                </div>
+                                <div class="display-flex-row gap-0p5">
+                                <a href="' . URL_ROOT . 'admins/employees/edit/' . $value->EmployeeId . '"> <i class="fa-solid fa-pen-to-square edit"></i></a>
+                                    <i class="fa-solid fa-trash-can delete" onclick="deleteEmployee(\'' . $value->EmployeeId . '\')"></i>
+                                </div>
                             </div>
-                            <div class="display-flex-row gap-0p5">
-                            <a href="' . URL_ROOT . 'admins/employees/edit/' . $value->EmployeeId . '"> <i class="fa-solid fa-pen-to-square edit"></i></a>
-                                <i class="fa-solid fa-trash-can delete" onclick="deleteEmployee(\'' . $value->EmployeeId . '\')"></i>
+                            <div class="cardhead">
+                                <div>
+                                    <div class="profile-name">' . $value->Firstname . ' ' . $value->Lastname . '</div>                                            
+                                    <div class="position">' . $value->Position . '</div>
+                                </div>
                             </div>
-                        </div>
-                        <div class="cardhead">
-                            <div>
-                                <div class="profile-name">' . $value->Firstname . ' ' . $value->Lastname . '</div>                                            
-                                <div class="position">' . $value->Position . '</div>
-                            </div>
-                        </div>
+                        </div>';
+                }
+            }
+
+            echo '</div>
+                </div>';
+        ?>
+
+        <?php
+            if ($data['supervisorDetail'] != null) {
+                echo '<div class="profile-detail-board  margin-bottom-4">
+                            <div class="profile-data-board">';
+
+                foreach ($data['supervisorDetail'] as $value) {
+
+                    echo '<div class="profile-card">
+                                <div class="display-flex-row justify-content-between">
+                                    <div class="profile-img">
+                                        <div style="background-image:url(' . URL_ROOT . 'public/images/profile/' . $value->Image . ')" class="width-rem-8p5 height-rem-8p5 background-image border-radius-11"></div>
+                                    </div>
+                                    <div class="display-flex-row gap-0p5">
+                                    <a href="' . URL_ROOT . 'admins/employees/edit/' . $value->EmployeeId . '"> <i class="fa-solid fa-pen-to-square edit"></i></a>
+                                        <i class="fa-solid fa-trash-can delete" onclick="deleteEmployee(\'' . $value->EmployeeId . '\')"></i>
+                                    </div>
+                                </div>
+                                <div class="cardhead">
+                                    <div>
+                                        <div class="profile-name">' . $value->Firstname . ' ' . $value->Lastname . '</div>                                            
+                                        <div class="position">' . $value->Position . '</div>
+                                    </div>
+                                </div>
+                            </div>';
+                }
+
+                echo '</div>
                     </div>';
             }
-        }
+        ?>
+
+        <?php
+            if ($data['assemblerDetail'] != null) {
+                echo '<div class="profile-detail-board  margin-bottom-4">
+                            <div class="profile-data-board">';
+
+                foreach ($data['assemblerDetail'] as $value) {
+
+                    echo '<div class="profile-card">
+                                <div class="display-flex-row justify-content-between">
+                                    <div class="profile-img">
+                                        <div style="background-image:url(' . URL_ROOT . 'public/images/profile/' . $value->Image . ')" class="width-rem-8p5 height-rem-8p5 background-image border-radius-11"></div>
+                                    </div>
+                                    <div class="display-flex-row gap-0p5">
+                                    <a href="' . URL_ROOT . 'admins/employees/edit/' . $value->EmployeeId . '"> <i class="fa-solid fa-pen-to-square edit"></i></a>
+                                        <i class="fa-solid fa-trash-can delete" onclick="deleteEmployee(\'' . $value->EmployeeId . '\')"></i>
+                                    </div>
+                                </div>
+                                <div class="cardhead">
+                                    <div>
+                                        <div class="profile-name">' . $value->Firstname . ' ' . $value->Lastname . '</div>                                            
+                                        <div class="position">' . $value->Position . '</div>
+                                    </div>
+                                </div>
+                            </div>';
+                }
+
+                echo '</div>
+                        </div>';
+
+            }
 
         ?>
 
-    </div>
-    </div>
-
-    <?php
-    if ($data['supervisorDetail'] != null) {
-        echo '<div class="profile-detail-board  margin-bottom-4">
+        <?php
+            if ($data['testerDetail'] != null) {
+                echo '<div class="profile-detail-board  margin-bottom-4">
                     <div class="profile-data-board">';
 
-        foreach ($data['supervisorDetail'] as $value) {
+                foreach ($data['testerDetail'] as $value) {
 
-            echo '<div class="profile-card">
-                        <div class="display-flex-row justify-content-between">
-                            <div class="profile-img">
-                                <div style="background-image:url(' . URL_ROOT . 'public/images/profile/' . $value->Image . ')" class="width-rem-8p5 height-rem-8p5 background-image border-radius-11"></div>
-                            </div>
-                            <div class="display-flex-row gap-0p5">
-                            <a href="' . URL_ROOT . 'admins/employees/edit/' . $value->EmployeeId . '"> <i class="fa-solid fa-pen-to-square edit"></i></a>
-                                <i class="fa-solid fa-trash-can delete" onclick="deleteEmployee(\'' . $value->EmployeeId . '\')"></i>
-                            </div>
+                    echo '<div class="profile-card">
+                                <div class="display-flex-row justify-content-between">
+                                    <div class="profile-img">
+                                        <div style="background-image:url(' . URL_ROOT . 'public/images/profile/' . $value->Image . ')" class="width-rem-8p5 height-rem-8p5 background-image border-radius-11"></div>
+                                    </div>
+                                    <div class="display-flex-row gap-0p5">
+                                    <a href="' . URL_ROOT . 'admins/employees/edit/' . $value->EmployeeId . '"> <i class="fa-solid fa-pen-to-square edit"></i></a>
+                                        <i class="fa-solid fa-trash-can delete" onclick="deleteEmployee(\'' . $value->EmployeeId . '\')"></i>
+                                    </div>
+                                </div>
+                                <div class="cardhead">
+                                    <div>
+                                        <div class="profile-name">' . $value->Firstname . ' ' . $value->Lastname . '</div>                                            
+                                        <div class="position">' . $value->Position . '</div>
+                                    </div>
+                                </div>
+                            </div>';
+                }
+            }
+
+            echo '</div>
+                </div>';
+        ?>
+
+
+        <div class="toolset-filterbox">
+            <div class="toolfilter">
+                <div class="filter-head">Filter by</div>
+                <div class="line"></div>
+                <div class="filters">
+
+
+                    <div class="filtertype">Vehicle Type</div>
+                    <form method="POST" action="<?php echo URL_ROOT; ?>admins/employees">
+                        <div class="filters">
+                            <input type="checkbox" id="manager" name="manager" value="Manager" <?php echo ($data['managerDetail'] == null) ? "" : "checked" ?>>
+                            <label for="manager">Manager</label>
                         </div>
-                        <div class="cardhead">
-                            <div>
-                                <div class="profile-name">' . $value->Firstname . ' ' . $value->Lastname . '</div>                                            
-                                <div class="position">' . $value->Position . '</div>
-                            </div>
+                        <div class="filters">
+                            <input type="checkbox" id="supervisor" name="supervisor" value="Supervisor" <?php echo ($data['supervisorDetail'] == null) ? "" : "checked" ?>>
+                            <label for="supervisor">Supervisor</label>
                         </div>
-                    </div>';
-        }
-    }
-
-    ?>
-
-    </div>
-    </div>
-
-    <?php
-    if ($data['assemblerDetail'] != null) {
-        echo '<div class="profile-detail-board  margin-bottom-4">
-                    <div class="profile-data-board">';
-
-        foreach ($data['assemblerDetail'] as $value) {
-
-            echo '<div class="profile-card">
-                        <div class="display-flex-row justify-content-between">
-                            <div class="profile-img">
-                                <div style="background-image:url(' . URL_ROOT . 'public/images/profile/' . $value->Image . ')" class="width-rem-8p5 height-rem-8p5 background-image border-radius-11"></div>
-                            </div>
-                            <div class="display-flex-row gap-0p5">
-                            <a href="' . URL_ROOT . 'admins/employees/edit/' . $value->EmployeeId . '"> <i class="fa-solid fa-pen-to-square edit"></i></a>
-                                <i class="fa-solid fa-trash-can delete" onclick="deleteEmployee(\'' . $value->EmployeeId . '\')"></i>
-                            </div>
+                        <div class="filters">
+                            <input type="checkbox" id="assembler" name="assembler" value="Assembler" <?php echo ($data['assemblerDetail'] == null) ? "" : "checked" ?>>
+                            <label for="assembler">Assembler</label>
                         </div>
-                        <div class="cardhead">
-                            <div>
-                                <div class="profile-name">' . $value->Firstname . ' ' . $value->Lastname . '</div>                                            
-                                <div class="position">' . $value->Position . '</div>
-                            </div>
+                        <div class="filters">
+                            <input type="checkbox" id="tester" name="tester" value="Tester" <?php echo ($data['testerDetail'] == null) ? "" : "checked" ?>>
+                            <label for="tester">Tester</label>
                         </div>
-                    </div>';
-        }
-    }
-
-    ?>
-
-    </div>
-    </div>
-
-    <?php
-    if ($data['testerDetail'] != null) {
-        echo '<div class="profile-detail-board  margin-bottom-4">
-            <div class="profile-data-board">';
-
-        foreach ($data['testerDetail'] as $value) {
-
-            echo '<div class="profile-card">
-                        <div class="display-flex-row justify-content-between">
-                            <div class="profile-img">
-                                <div style="background-image:url(' . URL_ROOT . 'public/images/profile/' . $value->Image . ')" class="width-rem-8p5 height-rem-8p5 background-image border-radius-11"></div>
-                            </div>
-                            <div class="display-flex-row gap-0p5">
-                            <a href="' . URL_ROOT . 'admins/employees/edit/' . $value->EmployeeId . '"> <i class="fa-solid fa-pen-to-square edit"></i></a>
-                                <i class="fa-solid fa-trash-can delete" onclick="deleteEmployee(\'' . $value->EmployeeId . '\')"></i>
-                            </div>
+                        <div class="filters">
+                            <button type="submit">Filter</button>
                         </div>
-                        <div class="cardhead">
-                            <div>
-                                <div class="profile-name">' . $value->Firstname . ' ' . $value->Lastname . '</div>                                            
-                                <div class="position">' . $value->Position . '</div>
-                            </div>
-                        </div>
-                    </div>';
-        }
-    }
 
-    ?>
-
-    </div>
-    </div>
-
-    <div class="toolset-filterbox">
-        <div class="toolfilter">
-            <div class="filter-head">Filter by</div>
-            <div class="line"></div>
-            <div class="filters">
-
-
-                <div class="filtertype">Vehicle Type</div>
-                <form method="POST" action="<?php echo URL_ROOT; ?>admins/employees">
-                    <div class="filters">
-                        <input type="checkbox" id="manager" name="manager" value="Manager" <?php echo ($data['managerDetail'] == null) ? "" : "checked" ?>>
-                        <label for="manager">Manager</label>
-                    </div>
-                    <div class="filters">
-                        <input type="checkbox" id="supervisor" name="supervisor" value="Supervisor" <?php echo ($data['supervisorDetail'] == null) ? "" : "checked" ?>>
-                        <label for="supervisor">Supervisor</label>
-                    </div>
-                    <div class="filters">
-                        <input type="checkbox" id="assembler" name="assembler" value="Assembler" <?php echo ($data['assemblerDetail'] == null) ? "" : "checked" ?>>
-                        <label for="assembler">Assembler</label>
-                    </div>
-                    <div class="filters">
-                        <input type="checkbox" id="tester" name="tester" value="Tester" <?php echo ($data['testerDetail'] == null) ? "" : "checked" ?>>
-                        <label for="tester">Tester</label>
-                    </div>
-                    <div class="filters">
-                        <button type="submit">Filter</button>
-                    </div>
-
+                </div>
             </div>
         </div>
-    </div>
     </div>
 
 </section>
