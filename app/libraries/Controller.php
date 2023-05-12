e38a5af0627b56a9d2555c8aac24d92741272955
Bhanuka Rajakaruna <87462362+BhanukaRajk@users.noreply.github.com>
2023-05-12 17:39:31 +0530

    Merge branch 'bhanuka' into Final

--- app/libraries/Controller.php
+++ app/libraries/Controller.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
     /* Controller Base | Load the models and views */
@@ -67,49 +66,4 @@
 
             return $array;
         }
-
-=======
-<?php
-
-    /* Controller Base | Load the models and views */
-
-    class Controller {
-
-        /* Model */
-        public function model( $model ) {
-
-            require_once '../app/models/' . $model . '.php';
-
-            return new $model();
-
-        }
-
-        /* View */
-        public function view( $view, $data = [] ) {
-
-            if( file_exists( '../app/views/' . $view . '.php' ) ) {
-
-                $data['url'] = getUrl();
-                require_once '../app/views/' . $view . '.php';
-
-            } else {
-
-                die( 'View does not exist' );
-
-            }
-
-        }
-
-        public function Sum($data, $str) : int {
-            $sum = 0;
-            if (empty($data)) {
-                return $sum;
-            }
-            foreach ($data as $value) {
-                $sum += $value->$str;
-            }
-            return $sum;
-        }
-
->>>>>>> bhanuka
     }
